99225a6eb6bee44227eab5deaa7338a78b27a56e
dokania-tanmay <dokania.tanmay@gmail.com>
2022-04-11 18:57:26 +0530

    Create elem.vhdl

---
+++ elem.vhdl
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.math_real.all;
+use ieee.numeric_std.all;
+
+-- Package Declarations
+package elem is
+    component reg_ent is
+        port(  );
+    end component;
+end package;
+
+-- Entity and Architecture Declarations
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
