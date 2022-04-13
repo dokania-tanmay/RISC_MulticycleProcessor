edb1e17e0e14a767dd90da92c1c6f420124d3faa
dokania-tanmay <dokania.tanmay@gmail.com>
2022-04-13 19:46:10 +0530

    RegisterFile done

--- registerFile.vhdl
+++ registerFile.vhdl
@@ -18,11 +18,17 @@ entity registerFile is
             clock, wr_enable, clear: in std_logic)
     );
 end entity;
-
+--- Writing is done at clock edges and changes to wr_enable signal
 architecture beh of registerFile is
-
-
-
-
-
+    type regBank is array(0 to numRegs-1) of std_logic_vector(dataSize-1 downto 0);
+    signal registers : regBank;
+begin
+    data_out1 <= registers(to_integer(unsigned(addr_out1)));
+    data_out2 <= registers(to_integer(unsigned(addr_out2)));
+    regProcess : process (clock, wr_enable) is
+    begin
+        if(wr_enable = '1') then
+            registers(to_integer(unsigned(addr_in))) <= data_in;
+        end if;
+    end process;
 end beh;
