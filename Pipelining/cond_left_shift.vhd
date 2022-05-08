9397cf34ee87334ffbac20ad126010777f549478
dokania-tanmay <dokania.tanmay@gmail.com>
2022-05-08 16:53:57 +0530

    Pipelined VHDL files

---
+++ Pipelining/cond_left_shift.vhd
@@ -0,0 +1,47 @@
+library ieee;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+use ieee.std_logic_1164.all;
+
+
+
+
+entity cond_left_shift is
+
+	port(
+		immediate: in std_logic_vector(8 downto 0);
+		opcode: in std_logic_vector(3 downto 0);
+      d3: in std_logic_vector(15 downto 0);
+      d3_out: out std_logic_vector(15 downto 0)
+                );
+		
+end entity;
+
+
+architecture cond_shifter of cond_left_shift is
+signal out_temp: std_logic_vector(15 downto 0);
+begin
+     
+     shifter:
+     process(opcode, immediate, d3)
+     begin
+        
+        case opcode is 
+           when "0100"  =>
+              out_temp(15 downto 7) <= immediate(8 downto 0);
+              out_temp(6 downto 0) <= "0000000";
+              
+            when others =>
+               
+              out_temp(15 downto 0) <= d3(15 downto 0);
+              
+              
+          end case;
+		
+		d3_out(15 downto 0) <= out_temp(15 downto 0);
+          
+      end process;
+      
+      
+  end architecture;
+                
---
+++ Pipelining/pc_pred.vhd
@@ -0,0 +1,62 @@
+library ieee;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+use ieee.std_logic_1164.all;
+
+
+
+
+entity pc_pred is
+
+	port(
+		pc_2, RB, alu_out: in std_logic_vector(15 downto 0);
+		opcode: in std_logic_vector(3 downto 0);
+                jump_enable: in std_logic;
+                pc_next: out std_logic_vector(15 downto 0)
+                );
+		
+end entity;
+
+
+architecture prediction of pc_pred is
+begin
+     
+     pred:
+     process(opcode, jump_enable, alu_out, pc_2, RB)
+     begin
+       
+       case opcode is 
+             when "1010" =>
+                pc_next(15 downto 0) <= RB(15 downto 0);
+                
+             when "1000" =>
+                if (jump_enable = '0') then
+                    pc_next(15 downto 0) <= pc_2(15 downto 0);
+                else
+                    pc_next(15 downto 0) <= alu_out(15 downto 0);
+                    
+                 end if ;
+             when "1011" | "1001" =>
+                    pc_next(15 downto 0) <= alu_out(15 downto 0);
+                    
+             when others => pc_next(15 downto 0) <= pc_2(15 downto 0) ;
+
+				  
+	    end case;
+		 
+
+		 
+              
+     end process ;
+       
+  end architecture;
+        
+              
+                      
+                 
+                     
+                 
+             
+       
+          
+
