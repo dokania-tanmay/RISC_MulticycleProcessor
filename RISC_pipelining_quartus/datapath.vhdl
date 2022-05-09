8c1953cb376d9bf8096be38c089ab9248bb7c20f
navneet-13 <87007888+navneet-13@users.noreply.github.com>
2022-05-10 04:19:49 +0530

    Update datapath.vhdl

--- RISC_pipelining_quartus/datapath.vhdl
+++ RISC_pipelining_quartus/datapath.vhdl
@@ -127,15 +127,16 @@ ARCHITECTURE flow OF datapath IS
 	signal WR_IF, WR_ID, WR_RR, WR_EX, WR_MEM, valid_IF, valid_out_IF, valid_ID, valid_out_ID, valid_RR, valid_out_RR, valid_EX, C_EX,
 			Z_EX, wb_control_EX
 			,valid_out_EX, C_out_EX, Z_out_EX, wb_control_out_EX,
-			valid_MEM, C_MEM, Z_MEM, wb_control_MEM,valid_out_MEM, C_out_MEM, Z_out_MEM, wb_control_out_MEM, flush, match, Addr_cmp1, Addr_cmp2 : STD_LOGIC; 
+			valid_MEM, C_MEM, Z_MEM, wb_control_MEM,valid_out_MEM, C_out_MEM, Z_out_MEM, wb_control_out_MEM, flush, match, Addr_cmp1, Addr_cmp2, Addr_cmp3, Addr_cmp4, Addr_cmp5, Addr_cmp6 : STD_LOGIC; 
 	signal pc_IF, pc_2_IF, inst_IF, pc_out_IF, pc_2_out_IF, inst_out_IF, pc_ID, pc_2_ID, inst_ID, pc_out_ID, pc_2_out_ID, 
 			inst_out_ID, pc_RR, pc_2_RR, inst_RR, pc_out_RR, pc_2_out_RR, inst_out_RR, D1_RR, D2_RR, D1_out_RR, D2_out_RR,
 			pc_EX, pc_2_EX, inst_EX, pc_out_EX, pc_2_out_EX, inst_out_EX, D1_EX, D3_EX, D1_out_EX, D3_out_EX, 
 			pc_MEM, pc_2_MEM, inst_MEM, pc_out_MEM, pc_2_out_MEM, inst_out_MEM, D3_MEM, D3_out_MEM, PC_Din, PC_next,PC_Dout, 
-			branch_addr, PC_M2_out, alu_1_out, R1_ALU_OPDR_SEL, R2_ALU_OPDR_SEL, RA_ALU, RB_ALU, Imm16: std_logic_vector(15 downto 0);
-	signal cond_ID, cond_out_ID, cond_RR, cond_out_RR, cond_EX, cond_out_EX, cond_MEM, cond_out_MEM, x: in std_logic_vector(1 downto 0);
+			branch_addr, PC_M2_out, alu_1_out, R1_ALU_OPDR_SEL, R2_ALU_OPDR_SEL, RA_ALU, RB_ALU, Imm16, RF_D1
+			, RF_D2: std_logic_vector(15 downto 0);
+	signal cond_ID, cond_out_ID, cond_RR, cond_out_RR, cond_EX, cond_out_EX, cond_MEM, cond_out_MEM: in std_logic_vector(1 downto 0);
 	signal AD1_ID, AD2_ID, AD3_ID, AD1_out_ID, AD2_out_ID, AD3_out_ID, AD1_RR, AD2_RR, AD3_RR, AD1_out_RR, AD2_out_RR, AD3_out_RR,
-			AD3_EX, AD3_out_EX, AD3_MEM, AD3_out_MEM: in std_logic_vector(2 downto 0);
+			AD3_EX, AD3_out_EX, AD3_MEM, AD3_out_MEM, x, y: in std_logic_vector(2 downto 0);
 	signal immd_RR, immd_out_RR, immd_EX, immd_out_EX, immd_MEM, immd_out_MEM: in std_logic_vector(9 downto 0);
 
 BEGIN
@@ -157,6 +158,7 @@ BEGIN
 	Alu_Oprd_sel : ALU_Oprd_Sel
 		port map(RA => R1_ALU_OPDR_SEL, RB => R2_ALU_OPDR_SEL, OPR1 => RA_ALU, OPR2 => RB_ALU, PC => pc_out_RR, immediate => Imm16, 
 			opcode => inst_out_RR(15 downto 12));
+
 	Prio_mux_1: priority_mux
 		port map(inp1 => D3_out_MEM, inp2 => D3_out_MEM, inp3 => D1_out_RR, sel => x , output => R1_ALU_OPDR_SEL);
 	Addr_cmp_1: Addr_cmp
@@ -164,7 +166,35 @@ BEGIN
 	x(1 downto 1) <= wb_control_out_MEM or Addr_cmp1;
 	Addr_cmp_2 : Addr_cmp
 		port map(addr1 => AD1_out_RR, addr2 => AD3_out_MEM, output_match => Addr_cmp2);
-	x(0 downto 0) <= wb_control_out_MEM or Addr_cmp2; 		
+	x(0 downto 0) <= wb_control_out_MEM or Addr_cmp2;
+
+
+	Prio_mux_2: priority_mux
+		port map(inp1 => D3_out_MEM, inp2 => D3_out_MEM, inp3 => D2_out_RR, sel => y , output => R2_ALU_OPDR_SEL);
+	Addr_cmp_3: Addr_cmp
+		port map(addr1 => AD2_out_RR, addr2 => AD3_out_EX, output_match => Addr_cmp3);
+	y(1 downto 1) <= wb_control_out_MEM or Addr_cmp3;
+	Addr_cmp_4 : Addr_cmp
+		port map(addr1 => AD2_out_RR, addr2 => AD3_out_MEM, output_match => Addr_cmp4);
+	y(0 downto 0) <= wb_control_out_MEM or Addr_cmp4;
+
+
+	Addr_cmp_5 : Addr_cmp
+		port map(addr1 => AD1_out_ID, addr2 => AD3_out_MEM, output_match => Addr_cmp5);
+	x(3 downto 3) <= wb_control_out_MEM or Addr_cmp5;
+	D1_RR <= RF_D1 when(x(3) = '0') else
+			D3_out_MEM;
+	
+	Addr_cmp_6 : Addr_cmp
+		port map(addr1 => AD2_out_ID, addr2 => AD3_out_MEM, output_match => Addr_cmp6);
+	y(3 downto 3) <= wb_control_out_MEM or Addr_cmp6;
+	D2_RR <= RF_D2 when(y(3) = '0') else
+			D3_out_MEM;
+
+
+
+
+
 ----------- Component Declaration
 	IF_ID_pipe : pipe_IFD
 		port map(pc=>pc_IF, pc_2=>pc_2_IF, inst=>inst_IF, valid=>valid_IF, clk => clock, clear=>(flush or reset), write_enable=>WR_IF, 
